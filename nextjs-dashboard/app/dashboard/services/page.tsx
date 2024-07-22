88adea1af370ecdfb8f721489f7948eef52fe75e
thomasdinh <work.dnstung@gmail.com>
2024-07-22 22:28:57 +0700

    complete chapter 5: Navigate between pages
    add seed data to project
    
    fix: target es2017

---
+++ nextjs-dashboard/app/dashboard/services/page.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+
+interface servicesProps {
+
+}
+
+const services: FC<servicesProps> = () : React.JSX.Element => {
+
+  return (
+    <div className='services'><h1>services</h1></div>
+    
+  );
+};
+
+export default services;

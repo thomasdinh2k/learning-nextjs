0e4727440f4571082ae952f3cb1759210208ed26
Thomas Dinh (Dinh Nguyen Son Tung) <work.dnstung@gmail.com>
2024-12-06 09:34:00 +0700

    optimize search by using debounce

--- nextjs-dashboard/app/ui/search.tsx
+++ nextjs-dashboard/app/ui/search.tsx
@@ -2,14 +2,14 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathName = usePathname();
   const { replace } = useRouter();
-  const handleSearch = (term: string) => {
-    console.log('🪳 ~ file: search.tsx:11 ~ handleSearch ~ term||', term);
 
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams)
     if (term) {
       params.set('query', term)
@@ -17,7 +17,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathName}?${params.toString()}`)
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">

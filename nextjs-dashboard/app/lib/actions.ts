a2e16a6109534da23536f594e8bd23e2093dc82f
Thomas Dinh <work.dnstung@gmail.com>
2024-12-06 16:54:45 +0700

    complete chapter 12: Mutating Data (#4)
    * remove all setTimeout(s)
    
    * link create invoice form
    
    * validate and prepare data
    
    * insert data to database
    
    * revalidate and redirect post form submission
    
    * complete logic to edit invoice
    
    * complete logic for delete invoices

---
+++ nextjs-dashboard/app/lib/actions.ts
@@ -0,0 +1,71 @@
+'use server';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { z } from 'zod';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(), //The amount field is specifically set to coerce (change) from a string to a number while also validating its type.
+
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+export async function createInvoice(formData: FormData) {
+  console.log('creating invoices', formData);
+  //   creating invoices FormData {
+  //     customerId: '13d07535-c59e-4157-a011-f8d2ef4e0cbb',
+  //     amount: '7',
+  //     status: 'paid'
+  //   }
+
+  //   ---> Manual Way
+  //   const rawFormData = {
+  //     customerId: formData.get('customerId'),
+  //     amount: formData.get('amount'),
+  //     status: formData.get('status'),
+  //   };
+
+  // ---> Using entries to quickly get all entries
+  const rawFormData = CreateInvoice.parse(
+    Object.fromEntries(formData.entries()),
+  );
+
+  const { customerId, amount, status } = rawFormData;
+  const amountInCents = amount * 1000;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse(
+    Object.fromEntries(formData.entries()),
+  );
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+  // Since the action is called directly in the path, `redirect` is not needed
+}
--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -17,8 +17,7 @@ export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :))
-
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -31,8 +30,7 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    await new Promise((resolve) => setTimeout(resolve, 4500));
-
+  // await new Promise((resolve) => setTimeout(resolve, 4500));
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices

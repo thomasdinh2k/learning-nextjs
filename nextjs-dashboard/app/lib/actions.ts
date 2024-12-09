de4e24b4f88505681a386ea793b9265828d17efb
Thomas Dinh (Dinh Nguyen Son Tung) <work.dnstung@gmail.com>
2024-12-09 13:40:10 +0700

    server-side validation logic for create invoice form

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -6,34 +6,45 @@ import { z } from 'zod';
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(), //The amount field is specifically set to coerce (change) from a string to a number while also validating its type.
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0' }), //The amount field is specifically set to coerce (change) from a string to a number while also validating its type.
 
-  status: z.enum(['pending', 'paid']),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
-export async function createInvoice(formData: FormData) {
-  //   creating invoices FormData {
-  //     customerId: '13d07535-c59e-4157-a011-f8d2ef4e0cbb',
-  //     amount: '7',
-  //     status: 'paid'
-  //   }
+export async function createInvoice(prevState: State, formData: FormData) {
+  // Validate form fields before sending to backend using Zod
+  const validateFields = CreateInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
 
-  //   ---> Manual Way
-  //   const rawFormData = {
-  //     customerId: formData.get('customerId'),
-  //     amount: formData.get('amount'),
-  //     status: formData.get('status'),
-  //   };
+  if (!validateFields.success) {
+    return {
+      errors: validateFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
 
-  // ---> Using entries to quickly get all entries
-  const rawFormData = CreateInvoice.parse(
-    Object.fromEntries(formData.entries()),
-  );
-
-  const { customerId, amount, status } = rawFormData;
+  const { customerId, amount, status } = validateFields.data;
   const amountInCents = amount * 1000;
   const date = new Date().toISOString().split('T')[0];
 

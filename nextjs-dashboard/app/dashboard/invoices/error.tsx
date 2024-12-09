a74f28c1e2ef39a0704f71cfd5de666aa64d8afd
Thomas Dinh (Dinh Nguyen Son Tung) <work.dnstung@gmail.com>
2024-12-09 10:14:37 +0700

    create error.tsx to handle error

---
+++ nextjs-dashboard/app/dashboard/invoices/error.tsx
@@ -0,0 +1,34 @@
+'use client'; // error.tsx needs to be a Client Component.
+
+import { useEffect } from 'react';
+
+export default function Error({
+    error, // This object is an instance of JavaScript's native Error object.
+    reset, // This is a function to reset the error boundary. When executed, the function will try to re-render the route segment.
+}: {
+    error: Error & { digest?: string };
+    reset: () => void;
+}) {
+    useEffect(() => {
+        // Optionally log the error to an error reporting service
+        console.error(error);
+    }, [error]);
+
+    console.log({ error, reset })
+
+    return (
+        <main className="flex h-full flex-col items-center justify-center">
+            <h2 className="text-center">Something went wrong!</h2>
+            <p>[{error["environmentName"]}] {error.message}</p>
+            <button
+                className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+                onClick={
+                    // Attempt to recover by trying to re-render the invoices route
+                    () => reset()
+                }
+            >
+                Try again
+            </button>
+        </main>
+    );
+}

fc2e5650cf8c98c179bc403427014f749c0bcc69
Thomas Dinh (Dinh Nguyen Son Tung) <work.dnstung@gmail.com>
2024-12-09 15:31:00 +0700

    add credentials provider

--- nextjs-dashboard/auth.ts
+++ nextjs-dashboard/auth.ts
@@ -1,6 +1,8 @@
 import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
 import { authConfig } from './auth.config';
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
+  providers: [Credentials({})],
 });

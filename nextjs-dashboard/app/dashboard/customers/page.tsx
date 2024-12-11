387291975cbde0f1b8ade95335cc54fc1057de6d
Thomas Dinh <work.dnstung@gmail.com>
2024-12-11 15:04:43 +0700

    complete chapter 16: adding metadata (#8)
    * update gitignore
    
    * add metadata to pages

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,13 +1,17 @@
+import { Metadata } from 'next';
 import React, { FC } from 'react';
 
+export const metadata: Metadata = {
+  title: "Customers"
+}
 
-const customers: FC = () : React.JSX.Element => {
+const customers: FC = (): React.JSX.Element => {
 
   return (
     <div className='test-dashboard'>
-        <h1>Customers</h1>
+      <h1>Customers</h1>
     </div>
-    
+
   );
 };
 
